73befa1d8979f04473f4a5b80ea530c441798522
Muhammad Amjad <muhammadamjad00@gmail.com>
2024-05-04 19:54:13 +0500

    h5 model added

--- fyp_backend/myapp/consumers/date_image_consumers.py
+++ fyp_backend/myapp/consumers/date_image_consumers.py
@@ -6,6 +6,8 @@ import json
 import tensorflow as tf
 import base64
 from pymongo import MongoClient
+import tensorflow as tf
+
 
 class_names = ['Ajwa', 'Galaxy', 'Jujube', 'Nabtat Ali', 'Rutab', 'Sabzo', 'Shaishe', 'Sokari', 'Sugaey']
 
@@ -24,7 +26,8 @@ class DateImageConsumer(AsyncWebsocketConsumer):
             if array.ndim == 2:
                 array = np.stack((array,) * 3, axis=-1)
             input_data = np.expand_dims(array, axis=0)
-            model = tf.keras.models.load_model('my_model_300_Epoch')
+            model = tf.keras.models.load_model('model.h5')
+            # model = tf.keras.layers.TFSMLayer("my_model_300_Epoch", call_endpoint='serving_default')
             predictions = model.predict(input_data)
             predicted_class_index = np.argmax(predictions)
             predicted_class_name = class_names[predicted_class_index]
