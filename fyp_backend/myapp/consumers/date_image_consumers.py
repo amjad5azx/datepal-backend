237a18896c1ec9069899495c5920a01d3a812d45
Muhammad Amjad <muhammadamjad00@gmail.com>
2024-05-08 03:46:14 +0500

    date iamge consumer updated

--- fyp_backend/myapp/consumers/date_image_consumers.py
+++ fyp_backend/myapp/consumers/date_image_consumers.py
@@ -19,35 +19,44 @@ class DateImageConsumer(AsyncWebsocketConsumer):
 
     async def receive(self, text_data=None, bytes_data=None):
         try:
-            if text_data:
-                split_data = text_data.split(",")
-                if len(split_data) > 1:
-                    decoded_data = base64.b64decode(split_data[1])  # Extract base64 data part
-                    image = Image.open(io.BytesIO(decoded_data))
-                    resized_image = image.resize((224, 224))
-                    array = np.array(resized_image)
-                    if array.ndim == 2:
-                        array = np.stack((array,) * 3, axis=-1)
-                    input_data = np.expand_dims(array, axis=0)
-                    model = tf.keras.models.load_model('model.h5')
-                    # model = tf.keras.layers.TFSMLayer("my_model_300_Epoch", call_endpoint='serving_default')
-                    predictions = model.predict(input_data)
-                    predicted_class_index = np.argmax(predictions)
-                    predicted_class_name = class_names[predicted_class_index]
-                    record = {"name": predicted_class_name}
-                    
-                    if record:
-                        await self.send(text_data=json.dumps(record))
-                    else:
-                        await self.send(text_data=json.dumps({'prediction': predicted_class_name, 'record': None}))
-                else:
-                    await self.send(text_data=json.dumps({'error': 'Invalid text_data format'}))
+            decoded_data = base64.b64decode(text_data)  # Extract base64 data part
+            image = Image.open(io.BytesIO(decoded_data))
+            resized_image = image.resize((224, 224))
+            array = np.array(resized_image)
+            if array.ndim == 2:
+                array = np.stack((array,) * 3, axis=-1)
+            input_data = np.expand_dims(array, axis=0)
+            model = tf.keras.models.load_model('model.h5')
+            # model = tf.keras.layers.TFSMLayer("my_model_300_Epoch", call_endpoint='serving_default')
+            predictions = model.predict(input_data)
+            predicted_class_index = np.argmax(predictions)
+            predicted_class_name = class_names[predicted_class_index]
+
+            uri = "mongodb+srv://amjad5azx:amjad1290@cluster0.imlsmty.mongodb.net/"
+
+            client = MongoClient(uri)
+            if client is not None:
+                print("Connected to MongoDB Atlas successfully.")
             else:
-                await self.send(text_data=json.dumps({'error': 'No text_data received'}))
+                print("Failed to connect to MongoDB Atlas.")
+
+            db = client["DateDB"]
+
+            collection_names = db.list_collection_names()
+            print("Available collections:", collection_names)
+                
+            collection=db[collection_names[0]]
+            record = collection.find_one({"name": predicted_class_name}, {"_id": 0})
+            # record = {"name": predicted_class_name}
+
+                
+            if record:
+                await self.send(text_data=json.dumps(record))
+            else:
+                await self.send(text_data=json.dumps({'prediction': predicted_class_name, 'record': None}))
         except Exception as e:
             print(f"Error processing image data: {e}")
             await self.send(text_data=json.dumps({'error': str(e)}))
-
 
     async def disconnect(self, close_code):
         print("WebSocket connection closed.")
