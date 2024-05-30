83179a9f4fa21326e4c4e0eeb58b8cf20c01f32d
Muhammad Amjad <muhammadamjad00@gmail.com>
2024-05-30 17:32:50 +0500

    settings.py updated

--- fyp_backend/fyp_backend/settings.py
+++ fyp_backend/fyp_backend/settings.py
@@ -86,7 +86,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("54.226.192.60", 6379)],
+            "hosts": [("18.206.209.122", 6379)],
         },
     },
 }
